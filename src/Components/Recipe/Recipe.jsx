9922c6a362b98dd3737fbfc1e7b39860b3f41d3a
NhsTarek <htarek275@gmail.com>
2024-03-18 00:51:49 +0600

    recipe image size fixed & recipe section finished

--- src/Components/Recipe/Recipe.jsx
+++ src/Components/Recipe/Recipe.jsx
@@ -1,32 +1,42 @@
 import { CiClock2 } from "react-icons/ci";
+import { GoFlame } from "react-icons/go";
 
 
 const Recipe = ({ recipe }) => {
     console.log(recipe);
-    const { recipe_name, recipe_image,short_description,ingredients,preparing_time } = recipe;
+    const { recipe_name, recipe_image, short_description, ingredients, preparing_time, calories } = recipe;
     return (
-        <div className="grid grid-cols-2 gap-4">
-            
-                <div className="card w-96 bg-base-100 shadow-xl">
-                    <figure className="px-10 pt-10">
-                        <img src={recipe_image} alt="Shoes" className="rounded-xl" />
-                    </figure>
-                    <div className="card-body items-center text-center">
-                        <h2 className="card-title">{recipe_name}</h2>
-                        <p className="border-b-2 py-4">{short_description}</p>
-                        <h6>Ingredients: {ingredients.length}</h6>
-                        <ul className="border-b-2 py-5">
-                            {
-                                  ingredients.map(ing =><li>{ing}</li>)
-                            }
-                        </ul>
-                        <div>
-                           <span><CiClock2 />{preparing_time}Min</span>
-                        </div>
+
+
+        <div className="card  bg-base-100 shadow-xl">
+            <figure className="px-10 pt-10">
+                <img src={recipe_image} alt="Shoes" className="rounded-xl max-w-full max-h-full" />
+            </figure>
+            <div className="card-body ">
+                <h2 className="card-title">{recipe_name}</h2>
+                <p className="border-b-2 py-4">{short_description}</p>
+                <h6>Ingredients: {ingredients.length}</h6>
+                <ul className="border-b-2 py-5 list-disc">
+                    {
+                        ingredients.map(ing => <li>{ing}</li>)
+                    }
+                </ul>
+                <div className="flex justify-between">
+                    <div className="flex gap-4 items-center">
+                        <span><CiClock2 /></span>
+                        <p>{preparing_time} Minutes</p>
                     </div>
+                    <div className="flex gap-4 items-center">
+                        <span><GoFlame /></span>
+                        <p>{calories} Calories</p>
+                    </div>
+                    
                 </div>
-            
+                <button className="bg-[#0BE58A] text-[#150B2B] font-medium rounded-full py-4 mx-20">Want to Cook</button>
+            </div>
         </div>
+
+
     );
 };
 
--- src/Components/Recipes/Recipes.jsx
+++ src/Components/Recipes/Recipes.jsx
@@ -4,19 +4,23 @@ import Recipe from "../Recipe/Recipe";
 
 const Recipes = () => {
     const [recipes, setRecipes] = useState([]);
-    useEffect(() =>{
+    useEffect(() => {
         fetch('recipes.json')
-        .then(res => res.json())
-        .then(data => setRecipes(data))
-    },[])
+            .then(res => res.json())
+            .then(data => setRecipes(data))
+    }, [])
     return (
         <div className="md:w-2/3">
             <h2 className="text-4xl">Recipes: {recipes.length}</h2>
-            {
-                recipes.map(recipe =><Recipe 
-                    recipe={recipe}
+            <div className="grid lg:grid-cols-2 gap-2">
+
+
+                {
+                    recipes.map(recipe => <Recipe
+                        recipe={recipe}
                     ></Recipe>)
-            }
+                }
+            </div>
         </div>
     );
 };
